733f553fba1d826d39c919960f5b428fc31329c4
Macartur Sousa <saasbook@saasbook.(none)>
2014-10-24 20:48:29 -0300

    Course_2 hw1

--- Course_2/oracle-of-bacon/lib/oracle_of_bacon.rb
+++ Course_2/oracle-of-bacon/lib/oracle_of_bacon.rb
@@ -20,11 +20,14 @@ class OracleOfBacon
   validate :from_does_not_equal_to
 
   def from_does_not_equal_to
-    # YOUR CODE HERE
+    errors.add(:base, "From cannot be the same as To") if @from == @to
   end
 
   def initialize(api_key='')
-    # your code here
+    @from = "Kevin Bacon"
+    @to = "Kevin Bacon"
+    @api_key=api_key
+    
   end
 
   def find_connections
